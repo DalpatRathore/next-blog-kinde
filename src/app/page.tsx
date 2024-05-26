fda50f1babfe390bac4c51201bbbc28f94c67a7d
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-05-26 18:12:49 +0530

    feat: landing page grid layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
+import { GridLayout } from "@/components/GridLayout";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="text-center pt-32 px-5">
-      <h1 className="text-4xl font-bold mb-5">Welcome to my Blog</h1>
-      <p className="max-w-4xl mx-auto leading-8">
+    <main className="text-center pt-20 px-5">
+      <h1 className="text-4xl font-bold mb-3">Welcome to my Blog</h1>
+      <p className="max-w-3xl mx-auto text-muted-foreground italic">
+        Laudantium accusamus eligendi nam, vero velit error architecto rem
+        magnam.
+      </p>
+      <GridLayout></GridLayout>
+      <p className="max-w-5xl mx-auto leading-8 mb-5 md:mb-10">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, maiores!
         Eius ut cum assumenda? Laudantium accusamus eligendi nam, vero velit
         error architecto rem magnam saepe cupiditate quas quos, temporibus
         excepturi. vero velit error architecto rem magnam saepe cupiditate quas
-        quos, temporibus excepturi.
+        quos, temporibus excepturi. Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Ea, maiores! Eius ut cum assumenda? Laudantium
+        accusamus eligendi nam, vero velit error architecto rem magnam saepe
+        cupiditate quas quos, temporibus excepturi. vero velit error architecto
+        rem magnam saepe cupiditate quas quos, temporibus excepturi.
       </p>
     </main>
   );

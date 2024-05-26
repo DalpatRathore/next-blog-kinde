ab281c84ae112b3242db559c141064d332511f67
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-05-26 18:48:04 +0530

    style: change twitter logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="bg-white dark:bg-gray-900">
+    <footer className="">
       <div className="mx-auto max-w-screen-xl pb-8">
-        <div className="border-t border-gray-100 pt-8 md:flex md:items-center md:justify-between  dark:border-gray-800 px-4 md:px-6 lg:px-8">
-          <p className="text-center md:text-left text-xs italic">
+        <div className="border-t pt-8 md:flex md:items-center md:justify-between px-4 md:px-6 lg:px-8">
+          <p className="text-center md:text-left text-xs italic text-muted-foreground">
             Built with React.js, Next.js, TypeScript, TailwindCSS, Shadcn UI, &
             Framer Motion.
           </p>
@@ -67,12 +67,16 @@ const Footer = () => {
                 <span className="sr-only">Twitter</span>
 
                 <svg
-                  className="h-6 w-6"
                   fill="currentColor"
-                  viewBox="0 0 24 24"
+                  x="0px"
+                  y="0px"
+                  className="h-6 w-6"
+                  width="100"
+                  height="100"
+                  viewBox="0 0 50 50"
                   aria-hidden="true"
                 >
-                  <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                  <path d="M 11 4 C 7.134 4 4 7.134 4 11 L 4 39 C 4 42.866 7.134 46 11 46 L 39 46 C 42.866 46 46 42.866 46 39 L 46 11 C 46 7.134 42.866 4 39 4 L 11 4 z M 13.085938 13 L 21.023438 13 L 26.660156 21.009766 L 33.5 13 L 36 13 L 27.789062 22.613281 L 37.914062 37 L 29.978516 37 L 23.4375 27.707031 L 15.5 37 L 13 37 L 22.308594 26.103516 L 13.085938 13 z M 16.914062 15 L 31.021484 35 L 34.085938 35 L 19.978516 15 L 16.914062 15 z"></path>
                 </svg>
               </a>
             </li>

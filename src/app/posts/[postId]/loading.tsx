9702dda74900ce310c1491b54d857266059cb01a
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-05-27 15:36:56 +0530

    style: post detail loading ui

--- src/app/posts/[postId]/loading.tsx
+++ src/app/posts/[postId]/loading.tsx
@@ -1,66 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Undo2 } from "lucide-react";
+
 const PostLoading = () => {
   return (
-    <div className="w-full max-w-5xl mx-auto pt-10">
-      <h2 className="text-2xl font-semibold mb-5 text-gray-500">
-        Post Details:
+    <div className="w-full max-w-3xl mx-auto py-10 px-5">
+      <h2 className="text-2xl md:text-3xl font-semibold mb-5 text-muted-foreground">
+        Post Details
       </h2>
-      <div role="status" className="space-y-5 animate-pulse max-w-5xl">
-        <div className="flex items-center w-full">
-          <div className="h-5 bg-gray-200 rounded-lg dark:bg-gray-700 w-32"></div>
-          <div className="h-5 ms-2 bg-gray-300 rounded-lg dark:bg-gray-600 w-24"></div>
-          <div className="h-5 ms-2 bg-gray-300 rounded-lg dark:bg-gray-600 w-full"></div>
+
+      <div className="flex flex-col space-y-3">
+        <Skeleton className="h-52 w-full rounded-xl" />
+        <div className="space-y-3">
+          <Skeleton className="h-10 w-full" />
+          <Skeleton className="h-6 w-32" />
+          <Skeleton className="h-24 w-full" />
         </div>
-        <div className="flex items-center w-full max-w-[480px]">
-          <div className="h-5 bg-gray-200 rounded-lg dark:bg-gray-700 w-full"></div>
-          <div className="h-5 ms-2 bg-gray-300 rounded-lg dark:bg-gray-600 w-full"></div>
-          <div className="h-5 ms-2 bg-gray-300 rounded-lg dark:bg-gray-600 w-24"></div>
-        </div>
-        <div className="flex items-center w-full max-w-[400px]">
-          <div className="h-5 bg-gray-300 rounded-lg dark:bg-gray-600 w-full"></div>
-          <div className="h-5 ms-2 bg-gray-200 rounded-lg dark:bg-gray-700 w-80"></div>
-          <div className="h-5 ms-2 bg-gray-300 rounded-lg dark:bg-gray-600 w-full"></div>
-        </div>
-        <div className="flex items-center w-full max-w-[480px]">
-          <div className="h-5 ms-2 bg-gray-200 rounded-lg dark:bg-gray-700 w-full"></div>
-          <div className="h-5 ms-2 bg-gray-300 rounded-lg dark:bg-gray-600 w-full"></div>
-          <div className="h-5 ms-2 bg-gray-300 rounded-lg dark:bg-gray-600 w-24"></div>
-        </div>
-        <div className="flex items-center w-full max-w-[440px]">
-          <div className="h-5 ms-2 bg-gray-300 rounded-lg dark:bg-gray-600 w-32"></div>
-          <div className="h-5 ms-2 bg-gray-300 rounded-lg dark:bg-gray-600 w-24"></div>
-          <div className="h-5 ms-2 bg-gray-200 rounded-lg dark:bg-gray-700 w-full"></div>
-        </div>
-        <div className="flex items-center w-full max-w-[360px]">
-          <div className="h-5 ms-2 bg-gray-300 rounded-lg dark:bg-gray-600 w-full"></div>
-          <div className="h-5 ms-2 bg-gray-200 rounded-lg dark:bg-gray-700 w-80"></div>
-          <div className="h-5 ms-2 bg-gray-300 rounded-lg dark:bg-gray-600 w-full"></div>
-        </div>
-        <span className="sr-only">Loading...</span>
       </div>
       <div className="flex items-center justify-center mt-10">
-        <button
-          className="group flex items-center justify-between gap-4 rounded-lg border border-gray-200 px-5 py-3 text-gray-200"
-          disabled
-        >
-          <span className="font-medium transition-colors">Back to Posts</span>
-
-          <span className="shrink-0 rounded-lg border  bg-white p-2 ">
-            <svg
-              className="size-5 rtl:rotate-180"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M17 8l4 4m0 0l-4 4m4-4H3"
-              />
-            </svg>
-          </span>
-        </button>
+        <Button size={"lg"} variant={"outline"} disabled>
+          Back to Posts <Undo2 className="w-5 h-5 ml-2 mb-1"></Undo2>
+        </Button>
       </div>
     </div>
   );
